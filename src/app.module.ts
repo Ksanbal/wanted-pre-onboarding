f983576dc63c90b15120465ea7e047d291b24fab
ksanbal <dev.ksanbal@gmail.com>
2022-08-17 17:26:42 +0900

    Feat. 프로젝트 파일구조 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { typeOrmModuleOptions } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { UserModule } from './api/user/user.module';
+import { CompanyModule } from './api/company/company.module';
+import { ApplyModule } from './api/apply/apply.module';
+import { JobsModule } from './api/jobs/jobs.module';
 
 @Module({
   imports: [
     // TypeORM
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    UserModule,
+    CompanyModule,
+    ApplyModule,
+    JobsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
