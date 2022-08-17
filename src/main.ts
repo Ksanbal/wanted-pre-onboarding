8c4619e3902c234de011b42609eef22e3df48e36
ksanbal <dev.ksanbal@gmail.com>
2022-08-17 16:51:09 +0900

    Feat. response, error response 구조 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // HttpException Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Response Interceptor
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
