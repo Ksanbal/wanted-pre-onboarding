cbe75db30345f4ae56ecc091c52e185cb8ccbfb9
ksanbal <dev.ksanbal@gmail.com>
2022-08-17 16:57:45 +0900

    Feat. swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
@@ -11,6 +12,29 @@ async function bootstrap() {
 
   // Response Interceptor
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // Swagger
+  SwaggerModule.setup(
+    '/docs',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Wanted Preonboarding')
+        .setDescription('원티드 프리온보딩 백엔드코스 4차 선발과제 API docs')
+        .setVersion('1.0')
+        .setContact(
+          'dev.ksanbal',
+          'https://devksanbal.site',
+          'dev.ksanbal@gmail.com',
+        )
+        .setLicense(
+          'MIT License',
+          'https://github.com/Ksanbal/wanted-pre-onboarding/blob/main/LICENSE',
+        )
+        .build(),
+    ),
+  );
 
   await app.listen(3000);
 }
