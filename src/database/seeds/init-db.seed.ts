1734a1ff9e0697ebaecd06097279461583e52271
ksanbal <dev.ksanbal@gmail.com>
2022-08-23 14:58:25 +0900

    Feat. seed 데이터 추가

--- src/database/seeds/init-db.seed.ts
+++ src/database/seeds/init-db.seed.ts
@@ -17,7 +17,10 @@ export class UserCreateSeed implements Seeder {
       .createQueryBuilder()
       .insert()
       .into(CompanyEntity)
-      .values([{ id: 1, name: '원티드', country: 'Korea', region: 'Seoul' }])
+      .values([
+        { id: 1, name: '원티드', country: 'Korea', region: 'Seoul' },
+        { id: 2, name: '왓챠', country: 'Korea', region: 'Seoul' },
+      ])
       .execute();
 
     await connection
@@ -33,6 +36,23 @@ export class UserCreateSeed implements Seeder {
           content:
             '원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..',
           techStack: 'NodeJS',
+        },
+        {
+          id: 2,
+          company: { id: 1, name: '원티드', country: 'Korea', region: 'Seoul' },
+          recruitPosition: 'Fronend',
+          recruitBonus: 500000,
+          content:
+            '원티드랩에서 프론트엔드 주니어 개발자를 채용합니다. 자격요건은..',
+          techStack: 'ReactJS',
+        },
+        {
+          id: 3,
+          company: { id: 2, name: '왓챠', country: 'Korea', region: 'Seoul' },
+          recruitPosition: 'Backend',
+          recruitBonus: 500000,
+          content: '왓챠에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..',
+          techStack: 'Spring',
         },
       ])
       .execute();
